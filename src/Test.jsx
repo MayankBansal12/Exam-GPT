805d4e09c66f1376ca647368c0deb4073d5f85d7
Mayank <mayankbansal125@gmail.com>
2023-09-29 12:24:52 +0530

    Add speech to text for user input

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,18 +1,37 @@
+import axios from 'axios';
 import React, { useState } from 'react';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
 const Test = () => {
-    const [speech, setSpeech] = useState(false);
-    const recording = () => {
-        setSpeech(true)
-    }
+    const [text, setText] = useState("");
+
+    const {
+        transcript,
+        listening,
+        browserSupportsSpeechRecognition
+    } = useSpeechRecognition();
+
+    const handleStartListening = () => SpeechRecognition.startListening({ continuous: true, interimResults: true });
+
+    const handleStopListening = () => {
+        SpeechRecognition.stopListening();
+        setText(transcript);
+        console.log(transcript);
+    };
     return (
         <div className="test-container">
             <h2>Exam Conversation</h2>
             <div className="conversation">
-                <div className="chat gpt">Say "I am ready to start" whenever you are ready to start the exam</div>
-                <div className="chat user">You: I am ready to start the exam</div>
-                <div className="chat gpt">Good, let's begin the exam on SQL databases. Question 1: Define SQL databases and explain their purpose in data management</div>
-                <div className="chat user" style={{ "textAlign": "center" }}>{!speech ? <span>Click on  <button className="material-symbols-outlined mic-btn" onClick={recording}>mic</button>  to start recording your answer</span> : "Recording..."}</div>
+                <div className="chats">
+                    <div className="chat gpt">Say "I am ready to start" whenever you are ready to start the exam</div>
+                    <div className="chat user">You: I am ready to start the exam</div>
+                    <div className="chat gpt">Good, let's begin the exam on SQL databases. Question 1: Define SQL databases and explain their purpose in data management</div>
+                </div>
+                {!browserSupportsSpeechRecognition ? <span>Browser doesn't support speech recognition.</span> : (<div className="chat user" style={{ "textAlign": "center" }}>
+                    {!listening ? <span>Click on  <button className="material-symbols-outlined mic-btn" onClick={handleStartListening} disabled={listening}>mic</button>  to start recording your answer</span> : <span>Recording...  Click <button className="material-symbols-outlined mic-btn" onClick={handleStopListening} disabled={!listening}>
+                        stop_circle
+                    </button> when you are done</span>}
+                </div>)}
             </div>
             <div style={{ "width": "100%", "textAlign": "center" }}>
                 <button className="end-btn"><span className="material-symbols-outlined">call_end</span></button>
