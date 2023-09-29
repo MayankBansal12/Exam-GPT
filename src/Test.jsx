f01267a6a115e757e1b5ee589511d22a35cc1ce2
Mayank <mayankbansal125@gmail.com>
2023-09-29 13:55:21 +0530

    Populate responses in conversation chats

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,23 +1,42 @@
 import axios from 'axios';
-import React from 'react';
+import React, { useState } from 'react';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
 const Test = () => {
+    const [chats, setChats] = useState([{
+        "role": "gpt",
+        "message": "Say 'I am ready to start' whenever you are ready to start the exam"
+    }]);
+
     const {
         transcript,
         listening,
         browserSupportsSpeechRecognition
     } = useSpeechRecognition();
 
-    const generateResponse = (input) => {
+    const generateResponse = async (input) => {
         console.log("Input Received:", input);
         const req = {
             input: input
         }
         if (input) {
-            axios.post('http://localhost:8000/response', req).then(res => {
-                console.log(res.data.response);
-            })
+            const userMessage = {
+                "role": "user",
+                "message": "You: " + input
+            };
+            setChats((prevChats) => [...prevChats, userMessage]);
+
+            try {
+                const res = await axios.post('http://localhost:8000/response', req);
+                const gptMessage = {
+                    "role": "gpt",
+                    "message": res.data.response
+                };
+                setChats((prevChats) => [...prevChats, gptMessage]);
+            } catch (error) {
+                // Handle any errors from the API request
+                console.error("API request error:", error);
+            }
         }
     }
 
@@ -32,12 +51,10 @@ const Test = () => {
         <div className="test-container">
             <h2>Exam Conversation</h2>
             <div className="conversation">
-                <div className="chats">
-                    <div className="chat gpt">Say "I am ready to start" whenever you are ready to start the exam</div>
-                    <div className="chat user">You: I am ready to start the exam</div>
-                    <div className="chat gpt">Good, let's begin the exam on SQL databases. Question 1: Define SQL databases and explain their purpose in data management</div>
-                </div>
-                {!browserSupportsSpeechRecognition ? <span>Browser doesn't support speech recognition.</span> : (<div className="chat user" style={{ "textAlign": "center" }}>
+                {chats.map((chat, i) => (
+                    <div key={i} className={"chat " + chat.role}>{chat.message}</div>
+                ))}
+                {!browserSupportsSpeechRecognition ? <span>Browser doesn't support speech recognition.</span> : (<div className="chat user" style={{ "textAlign": "center", "backgroundColor": "#252424" }}>
                     {!listening ? <span>Click on  <button className="material-symbols-outlined mic-btn" onClick={handleStartListening} disabled={listening}>mic</button>  to start recording your answer</span> : <span>Recording...  Click <button className="material-symbols-outlined mic-btn" onClick={handleStopListening} disabled={!listening}>
                         stop_circle
                     </button> when you are done</span>}
