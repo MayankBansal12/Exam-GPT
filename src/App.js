c0e9cd40cd18f5afed876fb45fc25c9f93405968
Mayank <mayankbansal125@gmail.com>
2023-10-01 20:23:08 +0530

    Set private route for test

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, { useContext } from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Test from "./components/Test";
-import { PdfTextProvider } from "./context/PdfTextContext";
+import { PdfTextProvider, TextContext } from "./context/PdfTextContext";
 
 const App = () => {
   return (
     <PdfTextProvider>
       <BrowserRouter>
         <Routes>
-          <Route exact path="/" Component={Home} />
-          <Route exact path="/test" Component={Test} />
+          <Route exact path="/" element={<Home />} />
+          <Route
+            path="/test"
+            element={<PrivateRoute path="/test" element={<Test />} />}
+          ></Route>
         </Routes>
       </BrowserRouter>
     </PdfTextProvider>
   );
+};
+
+const PrivateRoute = ({ element }) => {
+  const { pdfText } = useContext(TextContext);
+  if (!pdfText) {
+    return <Navigate to="/" />;
+  } else {
+    return element;
+  }
 };
 
 export default App;
